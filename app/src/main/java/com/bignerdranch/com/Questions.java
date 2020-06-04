bd8b76bfb8c5bbfdd756114e00919df9e1772f4d
mobin <mobinakhter18@gmail.com>
2020-06-04 00:34:44 -0400

    Create a Questions.java class, in which i added two field variables, initialized them inside a constructor, and added getters and setters for both of them THROUGH android studio. Changed preferences for field variables to have prefix of m, and static variables to have prefix of s. This way when i right click on a line, and click on Generate, it shows a list of thing that it'll generate for me.

---
+++ app/src/main/java/com/bignerdranch/com/Questions.java
@@ -0,0 +1,28 @@
+package com.bignerdranch.com;
+
+public class Questions {
+    // field/member/instance variable declaration
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Questions(int textResId, boolean answerTrue){
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
