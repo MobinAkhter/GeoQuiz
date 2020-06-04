6e7fc5ee17a3117fee094dd04e4a1256db18e3d2
mobin <mobinakhter18@gmail.com>
2020-06-03 23:58:05 -0400

    Add 2 field/instance/member variables. Also, get references to the widgets. Basically initialized the two variables to their appropriate "id" from the activity_quiz.xml Also, gave new "id's" to the two buttons inside the .xml file.

--- app/src/main/java/com/bignerdranch/com/MainActivity.java
+++ app/src/main/java/com/bignerdranch/com/MainActivity.java
@@ -3,12 +3,18 @@ package com.bignerdranch.com;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button mTrueButton; // Used m before the variable to indicate that these are member/instance variables.
+    private Button mFalseButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        mFalseButton = (Button) findViewById(R.id.false_button);
     }
 }
