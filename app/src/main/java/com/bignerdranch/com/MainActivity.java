595ed9c34d1092c7f3f174deecea909978711a7a
mobin <mobinakhter18@gmail.com>
2020-06-04 12:44:59 -0400

    Add two more variables in MainActivity.java, and added a Questions array, for them questions. Also, initialized a counter variable

--- app/src/main/java/com/bignerdranch/com/MainActivity.java
+++ app/src/main/java/com/bignerdranch/com/MainActivity.java
@@ -5,12 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Field variable declaration below
     private Button mTrueButton; // Used m before the variable to indicate that these are member/instance variables.
     private Button mFalseButton;
+    private Button mNextButton;
+    private TextView mQuestionTextView;
+
+    // Add a Questions Array
+    private Questions[] mQuestionBank = new Questions[] {
+            new Questions(R.string.question_oceans, true),
+            new Questions(R.string.question_mideast, false),
+            new Questions(R.string.question_africa, false),
+            new Questions(R.string.question_americas, true),
+            new Questions(R.string.question_asia, true)
+    };
+
+    private int mCurrentIndex = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
